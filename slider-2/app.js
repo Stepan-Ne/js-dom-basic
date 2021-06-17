c7dff716f221380e4ce07e32f59ea2f289d76b14
stepan <stepan.nebykov@prk-a.ru>
2021-06-17 15:46:17 +0300

    s2

--- slider-2/app.js
+++ slider-2/app.js
@@ -2,7 +2,9 @@ const upBtn = document.querySelector('.up-button')
 const downBtn = document.querySelector('.down-button')
 const sidebar = document.querySelector('.sidebar')
 const mainSlide = document.querySelector('.main-slide')
+const container = document.querySelector('.container')
 let slideCount = mainSlide.querySelectorAll('div').length
+let activeSlideIndex = 0
 
 sidebar.style.top = `-${(slideCount - 1) * 100}vh`
 
@@ -15,4 +17,18 @@ downBtn.addEventListener('click', () => {
 
 function changeSlide(direction) {
 
+  if (direction === 'up') {
+    activeSlideIndex++
+    if (activeSlideIndex === slideCount) {
+      activeSlideIndex = 0
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slideCount - 1
+    }
+  }
+  const height = container.clientHeight
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
 }
